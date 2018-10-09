49cf72522f32bb0063a4ac293a209141e0d5620c
PabloMesas <mesas.pablo@gmail.com>
2018-10-09 17:36:28 +0200

    Archivos Iniciales

---
+++ drowsiness/add.adb
@@ -0,0 +1,63 @@
+
+with Kernel.Serial_Output; use Kernel.Serial_Output;
+with Ada.Real_Time; use Ada.Real_Time;
+with System; use System;
+
+with Tools; use Tools;
+with Devices; use Devices;
+
+-- Packages needed to generate pulse interrupts       
+-- with Ada.Interrupts.Names;
+-- with Pulse_Interrupt; use Pulse_Interrupt;
+
+package body add is
+
+    -----------------------------------------------------------------------
+    ------------- declaration of tasks 
+    -----------------------------------------------------------------------
+
+    procedure Electrodes;
+
+    procedure Eyes_Detection; 
+
+    ----------------------------------------------------------------------
+    ------------- procedure exported 
+    ----------------------------------------------------------------------
+    procedure Background is
+    begin
+      loop
+        null;
+      end loop;
+    end Background;
+    ----------------------------------------------------------------------
+
+    ---------------------------------------------------------------------
+    Procedure Electrodes  is 
+        R: EEG_Samples_Type;
+    begin
+         Starting_Notice ("Electrodes"); 
+         Reading_Sensors (R);
+         Display_Electrodes_Sample (R);
+         Finishing_Notice ("Electrodes");
+    end Electrodes;
+
+    ---------------------------------------------------------------------
+    Procedure Eyes_Detection is
+        Current_R: Eyes_Samples_Type;
+    begin
+         Starting_Notice ("Eyes_Detection");
+         Reading_EyesImage (Current_R);
+         Display_Eyes_Sample (Current_R);
+         Beep (3);
+         Finishing_Notice ("Eyes_Detection");
+    end Eyes_Detection;
+
+
+begin
+   null;
+   Electrodes;
+   Eyes_Detection;
+end add;
+
+
+

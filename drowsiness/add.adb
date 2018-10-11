16ddd68dc25648c46dba39be90e1e0320fb0bcf0
PabloMesas <mesas.pablo@gmail.com>
2018-10-11 16:57:55 +0200

    Step 1 Conseguido Very Well Manuel!!!

--- drowsiness/add.adb
+++ drowsiness/add.adb
@@ -47,11 +47,21 @@ package body add is
         R: EEG_Samples_Type;
         next_time: Time;
         period : constant Time_Span := Milliseconds (300);
+        sum: Natural:= 0;
     begin
         next_time:= clock + period;
         loop
             Starting_Notice ("Start_Electrodes"); 
             Reading_Sensors (R);
+            sum := 0;
+            for i in 7..10 loop
+                sum := sum + Natural(R(EEG_Samples_Index(i)));
+            end loop;
+            if ( sum < 20 ) then
+                light(ON);
+            else 
+                light(OFF);
+            end if;
             Display_Electrodes_Sample (R);
             Finishing_Notice ("Finish_Electrodes");
 
@@ -63,6 +73,7 @@ package body add is
 
     ---------------------------------------------------------------------
     task body Eyes_Detection is
+        Counter: Integer := 0;
         Current_R: Eyes_Samples_Type;
         next_time: Time;
         period : constant Time_Span := Milliseconds (150);
@@ -71,8 +82,18 @@ package body add is
         loop
             Starting_Notice ("Start_Eyes_Detection");
             Reading_EyesImage (Current_R);
+            if (Current_R(left) < 20 and Current_R(right) < 20 ) then
+                Counter := Counter + 1;
+            else Counter := 0;
+            end if;
+
+            if (Counter = 2 ) then
+                Beep(2);
+            elsif (Counter > 3 )then
+                Beep(3);
+            end if;
+
             Display_Eyes_Sample (Current_R);
-    --        Beep (3);  --El conductor se despierta durante 2 segundos y luego cae.
             Finishing_Notice ("Finish_Eyes_Detection");
 
             delay until next_time;
